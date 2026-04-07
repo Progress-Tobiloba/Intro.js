8271f2e168389b044278a2895f17e5f2e36ef7ea
Progress-Tobiloba <progressosint@gmail.com>
2026-04-07 13:23:39 +0100

    Initial commit for Intro project

---
+++ Intro.js
@@ -0,0 +1,7 @@
+const name = "Progress";
+const age = 17; 
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
